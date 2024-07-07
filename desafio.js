a125fdc3b3509de45798e6b5588af7458746c3db
Hellen Silva <127620071+hellen-silvaa@users.noreply.github.com>
2024-07-07 00:48:52 -0300

    Update desafio.js

--- desafio.js
+++ desafio.js
@@ -158,6 +158,7 @@ if (idade >=18) {
     console.log("Você ainda é menor de idade");
 }
 
+
 // Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
 let numero = prompt("Digite um número: ")
 if (numero > 0) {
