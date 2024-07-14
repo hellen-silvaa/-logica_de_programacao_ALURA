46e5da0ea55210e2a85292086a96939bc3d1b737
Hellen Silva <127620071+hellen-silvaa@users.noreply.github.com>
2024-07-14 16:56:22 -0300

    Update desafio.js

--- desafio.js
+++ desafio.js
@@ -1,195 +1,216 @@
-//Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+// desafio 1: Interagindo com HTML
 
-alert("Boas vindas ao nosso site!");
+// Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
 
-//Declare uma variável chamada nome e atribua a ela o valor "Lua".
-let nome = 'Lua';
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
 
-// Crie uma variável chamada idade e atribua a ela o valor 25.
-let idade = 25;
+// Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
+// No index html:
+<button onclick ='exibirMensagemNoConsole()' class = 'button'>Console</button>
 
-// Defina uma variável numeroDeVendas e atribua a ela o valor 50.
-let numeroDeVendas = 50;
-
-// Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
-saldoDisponivel = 1000;
-
-// Exiba um alerta com o texto "Erro! Preencha todos os campos"
-alert("Erro! Preencha todos os campos");
-
-// Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos"
-//Agora exiba um alerta com o valor da variável mensagemDeErro.
-let mensagemDeErro = "Erro! Preencha todos os campos";
-alert(mensagemDeErro);
-
-// Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
-let seu_nome = prompt('Digite o seu nome: ');
-
-// Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
-let idade = prompt('Digite a sua idade: ');
-
-// Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
-if (idade >= 18) {
-    alert("Pode tirar a habilitação!")
+// no javascript:
+function exibirMensagemNoConsole(){
+    console.log('O botão foi clicado');
 }
 
+// Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
+// No index html:
+<button onclick="exibirAlerta()" class="button">Alert</button>
 
-//DESAFIOS 02
-
-Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
-dia_da_semana = prompt ('Digite o dia da semana: ');
-if (dia_da_semana == 'Sábado' ){
-    alert("Bom fim de semana!");
- }else if (dia_da_semana == 'Domingo' ){
-    alert('"Bom fim de semana!"');
- }else {
-    alert("Boa semana!");
+// no javascript:
+function exibirAlerta(){
+    alert('Eu amo JS');
 }
 
-// Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
-numero = prompt ('Digite um número: ');
-if (numero > 0 ){
-    alert('O número digitado é positivo!');
-} else {
-    alert('O número digitado é negativo!');
+// Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
+
+// No index html:
+<button onclick="exibirPrompt()" class="button">Prompt</button>
+
+// no javascript:
+function exibirPrompt(){
+    let nomeDaCidade = prompt('Digite o nome de uma cidade do Brasil');
+    alert(`Estive em ${nomeDaCidade} e lembrei de você`);
 }
 
+// Ao clicar no botão soma, peça 2 números inteiros e exiba o resultado da soma em um alerta.
+// No index html:
+<button onclick="somandoDoisNumeros()" class="button">Soma</button>
 
-// Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
-pontos = 105;
-if (pontos >= 100){
-    alert("Parabéns, você venceu!");
-}else{
-    alert("Tente novamente para ganhar.");
+// no javascript:
+function somandoDoisNumeros(){
+    let numero1 = parseInt(prompt('Digite o primeiro número'));
+    let numero2 = parseInt(prompt('Digite o segundo número'));
+    let resultado = numero1 + numero2;
+    alert(`O resultado da soma entre ${numero1} e ${numero2} é ${resultado}`);
 }
 
-// Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+// Criar uma função que exibe "Olá, mundo!" no console.
 
-let saldo_conta = 10.000;
-alert(`Seu saldo é ${saldo_conta}`);
+function exibirOla() {
+    console.log("Olá, mundo!");
+  }
+  
+  exibirOla();
 
-// Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+// Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
 
-nome = prompt('Digite seu nome: ');
-alert(`Seja bem vindo(a) ${nome}`);
+function exibirOlaNome(nome) {
+    console.log(`Olá, ${nome}!`);
+  }
+  
+  exibirOlaNome("Alice");
 
-// Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
-let contador = 1;
-while(contador <=10) {
-    console.log(contador);
-    contador++;
-}
+// Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
 
-// Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
-let contador = 10;
-while(contador >= 0) {
-    console.log(contador);
-    contador--;
-}
-// Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
-n_regressivo = prompt('Diga um número')
-while (n_regressivo >=0) {
-    console.log(contador);
-    contador--;
-}
+function calcularDobro(numero) {
+    return numero * 2;
+  }
+  
+  let resultadoDobro = calcularDobro(5);
+  console.log(resultadoDobro);
 
-let numeroMaximo = prompt("Digite um número para a contagem regressiva:");
-while (numeroMaximo >= 0) {
-    console.log(numeroMaximo);
-    numeroMaximo--;
-}
+// Criar uma função que recebe três números como parâmetros e retorna a média deles.
+
+function calcularMedia(a, b, c) {
+    return (a + b + c) / 3;
+  }
+  
+  let media = calcularMedia(4, 7, 10);
+  console.log(media);
+
+// Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+
+function encontrarMaior(a, b) {
+    return a > b ? a : b;
+  }
+  
+  let maiorNumero = encontrarMaior(15, 9);
+  console.log(maiorNumero);
+
+// Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+
+function quadrado(numero) {
+    return numero * numero;
+  }
+  
+  let resultado = quadrado(2);
+  console.log(resultado); 
+
+//   Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
 
 
-// Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
-n_progressivo = prompt('Diga um número')
-while (n_progressivo >=0) {
-    console.log(contador);
-    contador++;
+function calculaIMC(altura, peso){
+
+    let imc = peso / (alturaMetros * alturaMetros);
+  }
+  
+
+// Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+
+function calcularFatorial(numero) {
+    if (numero === 0 || numero === 1) {
+      return 1;
+    }
+  
+    let fatorial = 1;
+    for (let i = 2; i <= numero; i++) {
+      fatorial *= i;
+    }
+  
+    return fatorial;
+  }
+  
+  // Exemplo de uso
+  let numero = 5;
+  let resultado = calcularFatorial(numero);
+  console.log(`O fatorial de ${numero} é ${resultado}`);
+
+// Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
+
+function converterDolarParaReal(valorEmDolar) {
+    let cotacaoDolar = 4.80;
+    let valorEmReais = valorEmDolar * cotacaoDolar;
+    return valorEmReais.toFixed(2);
+  }
+  
+  // Exemplo
+  let valorEmDolar = 50;
+  let valorEmReais = converterDolarParaReal(valorEmDolar);
+  console.log(`${valorEmDolar} dólares equivalem a R$ ${valorEmReais}`);
+
+// Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.
+
+function calcularAreaPerimetroSalaRetangular(altura, largura) {
+    let area = altura * largura;
+    let perimetro = 2 * (altura + largura);
     
-}
-let numeroMaximo = prompt("Digite um número para a contagem progregressiva:");;
-let contador = 0;
+    console.log(`Área da sala: ${area} metros quadrados`);
+    console.log(`Perímetro da sala: ${perimetro} metros`);
+  }
+  
+  // Exemplo de uso
+  let altura = 3; // em metros
+  let largura = 5; // em metros
+  calcularAreaPerimetroSalaRetangular(altura, largura);
+  
 
-while (contador <= numeroMaximo) {
-    console.log(contador);
-    contador++
-}
+// Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
+function calcularAreaPerimetroSalaCircular(raio) {
+    let area = Math.PI * raio * raio;
+    let perimetro = 2 * Math.PI * raio;
+    
+    console.log(`Área da sala circular: ${area.toFixed(2)} metros quadrados`);
+    console.log(`Perímetro da sala circular: ${perimetro.toFixed(2)} metros`);
+  }
+  
+  // Exemplo de uso
+  let raio = 4; // em metros
+  calcularAreaPerimetroSalaCircular(raio);
 
-let numeroMaximo = prompt("Digite um número para a contagem progregressiva:");;
-let contador = 0;
 
-while (contador <= numeroMaximo) {
-    console.log(contador);
-    contador++
-}
+// Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
 
-// Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.
-console.log('Boas vindas');
-
-// Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
-nome = 'Hellen Silva';
-console.log(`Olá, ${nome}`);
-
-// Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .
-nome = 'Hellen Silva';
-alert(`Olá, ${nome}`);
-
-// Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
-let linguagemPreferida = prompt("Qual a linguagem de programação que você mais gosta?");
-console.log(linguagemPreferida);
-
-// Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é igual a [resultado]." no console.
-let valor1 = 10;
-let valor2 = 20;
-resultado = valor1 + valor2;
-console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}.`)
- 
-// Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a subtração desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A diferença entre [valor1] e [valor2] é igual a [resultado]." no console.
-let valor1 = 50;
-let valor2 = 20;
-resultado = valor1 - valor2;
-console.log(`A diferença entre ${valor1} e ${valor2} é igual a ${resultado}.`)
-
-// Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a pessoa é maior ou menor de idade, exibindo uma mensagem apropriada no console.
-let idade = prompt("Qual é a sua idade: ");
-if (idade >=18) {
-    console.log("Você é maior de idade!!!");
-}else {
-    console.log("Você ainda é menor de idade");
+function mostrarTabuada(numero) {
+  for (let i = 1; i <= 10; i++) {
+    let resultado = numero * i;
+    console.log(`${numero} x ${i} = ${resultado}`);
+  }
 }
 
+// Exemplo de uso
+let numero = 7;
+mostrarTabuada(numero);
 
-// Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
-let numero = prompt("Digite um número: ")
-if (numero > 0) {
-    console.log("O número é positivo");
-} else if (numero == 0){
-    console.log("O número é zero");
-}else{
-    console.log("O número é negativo");
-}
 
-// Use um loop while para imprimir os números de 1 a 10 no console.
-let numero =1;
-while(numero <=10){
-    console.log(numero);
-    numero++;
-}
-// Crie uma variável "nota" e atribua um valor numérico a ela. Use if-else para determinar se a nota é maior ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console.
-let nota = 6;
-if(nota >= 7){
-    console.log("Aprovado");
-}else{
-    console.log("Reprovado");
-}
-// Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
-let numeroAleatorio = Math.random();
-console.log(numeroAleatorio);
+// Crie uma lista vazia, com o nome listaGenerica.
 
-// Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
-let numeroInteiroAleatorio = parseInt (Math.random() * 10) + 1;
-console.log(numeroInteiroAleatorio);
+let listaGenerica = [];
 
-// Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
-let numeroAleatorio = parseInt (Math.random() * 1000) + 1;
-console.log(numeroInteiroAleatorio);
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+// Lista ao final: ['JavaScript', 'C', 'C++', 'Kotlin', 'Python', 'Java', 'Ruby', 'GoLang']
+
+
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[0]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[1]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[2]);
